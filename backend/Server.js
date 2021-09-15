bc5c83fc470ba7064b5b14d0ff9b3fa5dd41e0a0
Girlie Razon <77394701+girlierazon84@users.noreply.github.com>
2021-09-15 15:55:59 +0200

    Lade till CORS med inställningar

--- backend/Server.js
+++ backend/Server.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import cors from 'cors'
 
-dotenv.config()
-const app = express()
+dotenv.config ()
+const app = express ()
 const port = process.env.PORT
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use ( cors ( {
+    origin: '*',
+    methods: [ 'GET', 'POST', 'PUT', 'DELETE' ]
+} ) )
+app.use ( express.urlencoded ( { extended: false } ) )
+app.use ( express.json () )
 
 // Här finns databas relaterat
 let currentId = 14
 
-function incrementCurrentIdByOne() {
+function incrementCurrentIdByOne () {
     currentId += 1
 }
 
@@ -43,24 +48,24 @@ let inMemoryDataBase = [
 ]
 
 // Svar i kommunikation från API
-function messageUserNotFound() {
+function messageUserNotFound () {
     return {
         status: 404,
         text: 'User not found!'
     }
 }
 
-function messageSuccess(message) {
-    return  {
+function messageSuccess ( message ) {
+    return {
         status: 200,
         text: message
     }
 }
 
 // Sök i databasen
-function getUserIndex (id) {
-    for (let i = 0; i < inMemoryDataBase.length; i++) {
-        if (inMemoryDataBase[i].id === id) {
+function getUserIndex ( id ) {
+    for ( let i = 0; i < inMemoryDataBase.length; i++ ) {
+        if ( inMemoryDataBase[ i ].id === id ) {
             return i
         }
     }
@@ -68,97 +73,97 @@ function getUserIndex (id) {
 }
 
 // CRUD = Create = (POST), Read = (GET), Update = (PUT), Delete = (DELETE)
-function createNewUser(userData) {
+function createNewUser ( userData ) {
     let user = {
         id: currentId,
         name: userData.name,
         age: userData.age,
         gender: userData.gender,
     }
-    incrementCurrentIdByOne()
-    inMemoryDataBase.push(user)
+    incrementCurrentIdByOne ()
+    inMemoryDataBase.push ( user )
 }
 
-function getAllUsers() {
+function getAllUsers () {
     return inMemoryDataBase
 }
 
-function getUserById(id) {
-    let index = getUserIndex(id)
+function getUserById ( id ) {
+    let index = getUserIndex ( id )
 
-    if (index === -1) {
-        return messageUserNotFound()
+    if ( index === -1 ) {
+        return messageUserNotFound ()
     } else {
-        return messageSuccess(inMemoryDataBase[index])
+        return messageSuccess ( inMemoryDataBase[ index ] )
     }
 }
 
-function updateUser(userData) {
-    let index = getUserIndex(userData.id)
+function updateUser ( userData ) {
+    let index = getUserIndex ( userData.id )
 
-    if (index === -1) {
-        return messageUserNotFound()
+    if ( index === -1 ) {
+        return messageUserNotFound ()
     } else {
-        if (inMemoryDataBase[index].name !== userData.name) {
-            inMemoryDataBase[index].name = userData.name
+        if ( inMemoryDataBase[ index ].name !== userData.name ) {
+            inMemoryDataBase[ index ].name = userData.name
         }
-        if (inMemoryDataBase[index].age !== userData.age) {
-            inMemoryDataBase[index].age = userData.age
+        if ( inMemoryDataBase[ index ].age !== userData.age ) {
+            inMemoryDataBase[ index ].age = userData.age
         }
-        if (inMemoryDataBase[index].gender !== userData.gender) {
-            inMemoryDataBase[index].gender = userData.gender
+        if ( inMemoryDataBase[ index ].gender !== userData.gender ) {
+            inMemoryDataBase[ index ].gender = userData.gender
         }
-        return messageSuccess('User updated!')
+        return messageSuccess ( 'User updated!' )
     }
 }
 
-function deleteUser(index) {
-    inMemoryDataBase.splice(index, 1)
+function deleteUser ( index ) {
+    inMemoryDataBase.splice ( index, 1 )
 }
 
-function deleteUserById(id) {
-    let index = getUserIndex(id)
+function deleteUserById ( id ) {
+    let index = getUserIndex ( id )
 
-    if (index === -1) {
-        return messageUserNotFound()
+    if ( index === -1 ) {
+        return messageUserNotFound ()
     } else {
-        deleteUser(index)
-        return messageSuccess('User deleted successfully!')
+        deleteUser ( index )
+        return messageSuccess ( 'User deleted successfully!' )
     }
 }
 
 // Kontrollerar att APIet lever
-app.get('/', function (req,res) {
-    res.send('API is Alive!')
-})
+app.get ( '/', function ( req, res ) {
+    res.send ( 'API is Alive!' )
+} )
 
 // API CRUD = Create = (POST), Read = (GET), Update = (PUT), Delete = (DELETE)
-app.post('/users', function (req, res) {
-    createNewUser(req.body)
-    res.json('Successfully created a new user')
-})
+app.post ( '/users', function ( req, res ) {
+    createNewUser ( req.body )
+    res.json ( 'Successfully created a new user' )
+} )
 
-app.get('/users', function (req, res) {
-    res.json(getAllUsers())
-})
+app.get ( '/users', function ( req, res ) {
+    res.json ( getAllUsers () )
+} )
 
-app.get('/users/:id', function (req, res) {
-    const id = Number(req.params.id)
-    let response = getUserById(id)
-    res.status(response.status).json(response.text)
-})
+app.get ( '/users/:id', function ( req, res ) {
+    const id = Number ( req.params.id )
+    let response = getUserById ( id )
+    res.status ( response.status ).json ( response.text )
+} )
 
-app.put('/users', function (req, res) {
-    let response = updateUser(req.body)
-    res.status(response.status).send(response.text)
-})
+app.put ( '/users', function ( req, res ) {
+    let response = updateUser ( req.body )
+    res.status ( response.status ).send ( response.text )
+} )
 
-app.delete('/users/:id', function (req, res) {
-    let response = deleteUserById(Number(req.params.id))
-    res.status(response.status).send(response.text)
-});
+app.delete ( '/users/:id', function ( req, res ) {
+    let response = deleteUserById ( Number ( req.params.id ) )
+    res.status ( response.status ).send ( response.text )
+} );
 
 // Måste lägga till alltid i sist = Startar servern
-app.listen(port, () => {
-    console.log(`The server is running on port ${ port }`)
-})
+app.listen ( port, () => {
+    console.log ( `The server is running on port ${ port }` )
+} )
